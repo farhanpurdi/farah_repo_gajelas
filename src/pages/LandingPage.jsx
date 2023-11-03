fee0828f7c1333c3a04bf9b7bedf5b44351c3414
Farhan Purdi <farhanpurdiihsani@gmail.com>
2023-11-03 23:53:21 +0700

    update

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,6 +1,6 @@
 import Footer from "../components/Footer"
 import Header from "../components/Header/index"
-import Navbar from "../components/Navbar/index"
+import Navbar from "../components/Navbar/Index"
 
 const LandingPage = () => {
 
