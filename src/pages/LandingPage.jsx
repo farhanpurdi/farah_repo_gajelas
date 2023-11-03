71c842d601267a80b4f52f4fde7f0100b2c30e51
Farhan Purdi <farhanpurdiihsani@gmail.com>
2023-11-03 22:48:44 +0700

    update

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,14 +1,15 @@
-import Footer from "../components/Footer"
-import Header from "../components/Header"
-import Navbar from "../components/Navbar/Index"
+// import Footer from "../components/Footer"
+// import Header from "../components/Header"
+// import Navbar from "../components/Navbar/Index"
+import { Footer, Header, Navbar } from "../components";
 
 const LandingPage = () => {
 
     return(
         <>
-        <Navbar />
-        <Header />
-        <Footer />
+        <Navbar.Navbar />
+        <Header.Header />
+        <Footer.Footer />
         </>
     )
 
